300d4eb9d75c58d2b453ec277b3bc08af3ba74e5
josseLara <josselara1@gmail.com>
2023-09-14 21:03:28 -0300

    ca

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
 
   return (
     <>
-     <h1 className='text-red'>as</h1>
+     <h1 className='text-blue-600 font-light'>as</h1>
     </>
   )
 }
